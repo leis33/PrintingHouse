8277a8b5905f2ba5af8a4eee619ca2184a866ec4
leis33 <l.abdulah@abv.bg>
2025-05-23 21:24:40 +0300

    feat: add exception type and messages

---
+++ src/main/java/org/exercises/exceptions/ExceptionMessages.java
@@ -0,0 +1,6 @@
+package org.exercises.exceptions;
+
+public class ExceptionMessages {
+    public static final String UNSUPPORTED_COLOR_PRINT = "This machine does not support color printing.";
+    public static final String INSUFFICIENT_PAPER = "Not enough paper is loaded.";
+}
---
+++ src/main/java/org/exercises/exceptions/PrintException.java
@@ -0,0 +1,7 @@
+package org.exercises.exceptions;
+
+public class PrintException extends Exception {
+    public PrintException(String message) {
+        super(message);
+    }
+}
